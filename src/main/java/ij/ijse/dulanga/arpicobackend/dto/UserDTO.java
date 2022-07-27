4abba9245054b0d324d49b4d051e5d269afba014
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-07-28 00:24:31 +0530

    Create UserDTO and validated using Hibernate Validator

--- src/main/java/ij/ijse/dulanga/arpicobackend/dto/UserDTO.java
+++ src/main/java/ij/ijse/dulanga/arpicobackend/dto/UserDTO.java
@@ -1,4 +1,39 @@
 package ij.ijse.dulanga.arpicobackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserDTO {
+
+    @NotNull(message = "Username shouldn't be empty")
+    private String username;
+
+    @NotNull(message = "password can not be empty")
+    @Length(min = 6, message = "Password should have at least 6 characters")
+    private String password;
+
+    @NotNull(message = "Full name can not be empty")
+    @Pattern(regexp = "[A-Za-z ]+")
+    private String fullName;
+
+    @NotNull(message = "Address can not be empty")
+    @Pattern(regexp = "[A-Za-z\\d]{5,}", message = "Invalid address Input")
+    private String address;
+
+    @NotNull(message = "Age can not be empty")
+    @Digits(message = "Invalid age input", integer = 2, fraction = 0)
+    private Integer age;
+
+    @Email(message = "Invalid Email")
+    @NotNull(message = "Email can not be empty")
+    private String email;
+
 }
+
