2c47a08409305c53d9dd756ea06ef1aad2c5e58e
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-07-28 10:10:45 +0530

    Implemented the UserService interface, DuplicateEmailException and NotFoundException

--- src/main/java/ij/ijse/dulanga/arpicobackend/service/UserService.java
+++ src/main/java/ij/ijse/dulanga/arpicobackend/service/UserService.java
@@ -1,5 +1,15 @@
 package ij.ijse.dulanga.arpicobackend.service;
 
+import ij.ijse.dulanga.arpicobackend.dto.UserDTO;
+import ij.ijse.dulanga.arpicobackend.service.exception.DuplicateEmailException;
+import ij.ijse.dulanga.arpicobackend.service.exception.NotFoundException;
+
 public interface UserService {
+    UserDTO registerUser(UserDTO user) throws DuplicateEmailException;
+
+    void updateUSer(UserDTO user) throws NotFoundException;
+
+    UserDTO getUserInfo(String userId) throws NotFoundException;
+    void deleteUser(String userId) throws NotFoundException;
 
 }
--- src/main/java/ij/ijse/dulanga/arpicobackend/service/impl/UserServiceImpl.java
+++ src/main/java/ij/ijse/dulanga/arpicobackend/service/impl/UserServiceImpl.java
@@ -1,10 +1,32 @@
 package ij.ijse.dulanga.arpicobackend.service.impl;
 
+import ij.ijse.dulanga.arpicobackend.dto.UserDTO;
 import ij.ijse.dulanga.arpicobackend.service.UserService;
+import ij.ijse.dulanga.arpicobackend.service.exception.DuplicateEmailException;
+import ij.ijse.dulanga.arpicobackend.service.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
+    @Override
+    public UserDTO registerUser(UserDTO user) throws DuplicateEmailException {
+        return null;
+    }
+
+    @Override
+    public void updateUSer(UserDTO user) throws NotFoundException {
+
+    }
+
+    @Override
+    public UserDTO getUserInfo(String userId) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String userId) throws NotFoundException {
+
+    }
 }
