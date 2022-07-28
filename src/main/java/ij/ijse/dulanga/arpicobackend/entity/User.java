431cd0fc148d4ddcc287444477237260e48af464
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-07-28 09:55:52 +0530

    Create the User Entity

---
+++ src/main/java/ij/ijse/dulanga/arpicobackend/entity/User.java
@@ -0,0 +1,34 @@
+package ij.ijse.dulanga.arpicobackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements SuperEntity {
+
+    @Id
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false,name = "full_name")
+    private String fullName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String email;
+}
