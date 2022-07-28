7953da93bd45f8723cdee83d36d92e3a8733bea2
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-07-28 11:20:04 +0530

    Implement the registerUser in UserController

--- src/main/java/ij/ijse/dulanga/arpicobackend/api/UserController.java
+++ src/main/java/ij/ijse/dulanga/arpicobackend/api/UserController.java
@@ -4,8 +4,10 @@ import ij.ijse.dulanga.arpicobackend.dto.UserDTO;
 import ij.ijse.dulanga.arpicobackend.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -22,8 +24,12 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO registerUser(@RequestBody @Validated UserDTO user) {
-        return null;
+    public UserDTO registerUser(@RequestBody @Validated UserDTO user, Errors errors) {
+        if (errors.hasFieldErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.getFieldErrors().get(0).getDefaultMessage());
+        }
+        userService.registerUser(user);
+        return user;
     }
 
     @GetMapping(path = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
